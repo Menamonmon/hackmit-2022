f1caca693aa5910e47020bb879dd8fb225315603
Ervin Grimaldi <grimaldiervin@gmail.com>
2022-10-01 21:29:45 -0700

    Add files via upload
    Initially, modified news_scrapping.py and created a flask localhost server for displaying news link in form of API

--- backend/news_scraping.py
+++ backend/news_scraping.py
@@ -11,11 +11,10 @@ def gain_links_ap_news(url):
     for element in links_non_filtered:
         if 'href="/article/' in str(element):
             index, fine = str(element).find('href="/article/'), str(element).find('">')
-            lista.append("https://apnews.com"+str(element)[index+6:fine+1])
+            if "https://apnews.com"+str(element)[index+6:fine+1] not in lista:
+                lista.append("https://apnews.com"+str(element)[index+6:fine+1])
     return lista
 
 
-
-if __name__ == "__main__":
-    politics_list = gain_links_ap_news("https://apnews.com/hub/politics?utm_source=apnewsnav&utm_medium=navigation")
-    sports_list = gain_links_ap_news("https://apnews.com/hub/sports?utm_source=apnewsnav&utm_medium=navigation")
+politics_list = gain_links_ap_news("https://apnews.com/hub/politics?utm_source=apnewsnav&utm_medium=navigation")
+sports_list = gain_links_ap_news("https://apnews.com/hub/sports?utm_source=apnewsnav&utm_medium=navigation")
